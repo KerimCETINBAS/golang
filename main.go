554b17cf76dbb4becb40d14368caacdb0adad147
Kerim Cetinbas <a.kerim.cetinbas@yandex.com>
2023-06-02 04:18:28 +0300

    interface

--- main.go
+++ main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import "fmt"
+
+type creature interface {
+	getName() string
+}
+
+type monster struct {
+	name string
+	race string
+}
+type character struct {
+	name string
+	race string
+}
+
+func (c character) getName() string {
+	return c.name
+}
+
+func (c monster) getName() string {
+	return c.name
+}
+
+func printName(c creature) {
+	fmt.Println(c.getName())
+}
+
+func main() {
+
+	newMonster := monster{
+		name: "goblin slayer",
+		race: "hobgoblin",
+	}
+
+	printName(newMonster)
+}
